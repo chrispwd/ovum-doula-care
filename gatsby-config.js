672a97600fd02ea0f4dd8c0090a64a7913c82be1
chrispwd <chrispwd@protonmail.com>
2023-05-12 23:41:46 -0600

    init commit

---
+++ gatsby-config.js
@@ -0,0 +1,63 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`
+});
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+    siteMetadata: {
+        title: `Gatsby Default Starter`,
+        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        author: `@gatsbyjs`,
+        siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    },
+    plugins: [
+        {
+            resolve: "gatsby-source-strapi",
+            options: {
+                apiURL: process.env.STRAPI_API_URL,
+                accessToken: process.env.STRAPI_TOKEN,
+                collectionTypes: [
+                    "testimonial",
+                    {
+                        name: "birth-package",
+                        i18n: { locales: "all" },
+                    }
+                ],
+                singleTypes: [],
+                queryLimit: 1000,
+            },
+        },
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                // This will impact how browsers show your PWA/website
+                // https://css-tricks.com/meta-theme-color-and-trickery/
+                // theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-postcss`,
+    ],
+}
---
+++ src/pages/testimonials.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const TestimonialsPage = () => (
+  <Layout>
+    <h1>404: Not Found</h1>
+    <p>Page not found.</p>
+  </Layout>
+)
+
+export const Head = () => <Seo title="Testimonials" />
+
+export default TestimonialsPage
